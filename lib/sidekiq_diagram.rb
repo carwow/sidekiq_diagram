447dd048b8e381cea205ae9c5152c9bf4ee58e47
Richard Vickerstaff <richard.vickerstaff@carwow.co.uk>
2023-04-04 10:16:14 +0100

    Init

---
+++ lib/sidekiq_diagram.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sidekiq_diagram/version"
+
+module SidekiqDiagram
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sidekiq_diagram_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SidekiqDiagram do
+  it "has a version number" do
+    expect(SidekiqDiagram::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
